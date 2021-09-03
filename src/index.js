f04f5955da19b07be78fd6a51f069dedcc92d1db
elmar8287 <L-mar@inbox.ru>
2021-09-03 17:20:21 +0400

    Fix page reload and scores displaying

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const gameURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/8CJbUotFazwQ3FpCyxsp/scores';
+let scoreResult = [];
 
 const scoreListDisplay = (scores) => scores.map((score) => `<li class="list-group-item d-flex justify-content-between align-items-start">${score.user}:${score.score}</li>`).join('');
 
@@ -15,8 +17,8 @@ const createGame = async (gameName) => {
   return gameId;
 };
 
-const addScore = async (userName, gameId) => {
-  const response = await fetch(`${baseURL}games/${gameId}/scores/`, {
+const addScore = async (userName, gameURL) => {
+  const response = await fetch(gameURL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -28,10 +30,11 @@ const addScore = async (userName, gameId) => {
   return result;
 };
 
-const getScore = async (gameId) => {
-  const response = await fetch(`${baseURL}games/${gameId}/scores/`);
+const getScore = async (gameURL) => {
+  const response = await fetch(gameURL);
   const result = await response.json();
   return result;
+
 };
 
 const form = document.querySelector('#form');
@@ -40,19 +43,23 @@ const refreshButton = document.querySelector('#refreshBtn');
 const user = document.querySelector('#name');
 const score = document.querySelector('#score');
 
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const userName = { user: user.value, score: score.value };
+  await addScore(userName, gameURL);
+  user.value = '';
+  score.value = '';
+});
+
+refreshButton.addEventListener('click', async () => {
+  const result = await getScore(gameURL);
+  scoreList.innerHTML = scoreListDisplay(result.result);
+});
+
 window.addEventListener('DOMContentLoaded', async () => {
   const game = { name: 'My cool new game' };
   const { result } = await createGame(game);
-  const gameId = result.split(' ')[2];
 
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const userName = { user: user.value, score: score.value };
-    await addScore(userName, gameId);
-  });
-
-  refreshButton.addEventListener('click', async () => {
-    const result = await getScore(gameId);
-    scoreList.innerHTML = scoreListDisplay(result.result);
-  });
+  const refreshResult = await getScore(gameURL);
+  scoreList.innerHTML = scoreListDisplay(refreshResult.result);
 });
