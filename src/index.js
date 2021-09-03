00b280bcbc28d78b0fb133b1f71adcc34124e44c
elmar8287 <L-mar@inbox.ru>
2021-09-03 10:20:45 +0400

    Add add score functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
+const scoreListDisplay = (scores) => scores.map((score) => `<li><span>${score.user}</span><span> ${score.score}</span></li>`).join('');
+
 const createGame = async (gameName) => {
   const response = await fetch(`${baseURL}games/`, {
     method: 'POST',
@@ -11,4 +13,17 @@ const createGame = async (gameName) => {
 
   const gameId = await response.json();
   return gameId;
+};
+
+const addScore = async (user_1, gameId) => {
+  const response = await fetch(`${baseURL}games/${gameId}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user_1),
+  });
+
+  const result = await response.json();
+  return result;
 };
