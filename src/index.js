00f0f086c2755d5109f06c71e36f0a2f50dd6006
elmar8287 <L-mar@inbox.ru>
2021-09-03 12:06:19 +0400

    Create add score section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
 const scoreListDisplay = (scores) => scores.map((score) => `<li class="list-group-item list-group-item-action">${score.user}:${score.score}</li>`).join('');
@@ -15,5 +14,3 @@ const createGame = async (gameName) => {
   const gameId = await response.json();
   return gameId;
 };
-=======
->>>>>>> 441e1894c49285a5f443dbca439e1165e044622a
